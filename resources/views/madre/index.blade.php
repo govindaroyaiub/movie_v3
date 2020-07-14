a7d787387e435f5191cee5d1c4c28631bbbba248
ebnsina <ebnsina.me@gmail.com>
2020-07-14 14:24:17 +0600

    slider responsive fix

--- resources/views/madre/index.blade.php
+++ resources/views/madre/index.blade.php
@@ -327,31 +327,6 @@
             autoplaySpeed: 5000,
             slidesToShow: 1,
             slidesToScroll: 1,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: true
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
         });
     });
 </script>
